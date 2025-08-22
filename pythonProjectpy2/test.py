8a47f5fb8f6309c32b852c8846e350431f8265a3
张云腾 <15234711471@163.com>
2025-08-22 22:18:20 +0800

    python基础语法2

---
+++ pythonProjectpy2/test.py
@@ -0,0 +1,70 @@
+# while循环
+# 1.
+# i = 1
+# while i <= 100:
+#     print('hello world', i)
+#     i += 1
+# # 2.while死循环
+# # while True:
+# #     print('hello world', i)
+# # 计算1+2+3+4到100的和
+# sum1 = 0
+# j = 1
+# while j <= 100:
+#     sum1 += j
+#     j += 1
+# print(sum1)     # 顶格打印
+#
+# # 循环嵌套
+# h = 1
+# while h <= 3:
+#     print("外循环", h)
+#     h += 1
+#     k = 1
+#     while k <= 5:
+#         print("内循环", k)
+#         k += 1
+#
+# # for循环
+# str2 = 'helloworld'
+# for x in str2:
+#     print(x)
+#
+# # range()函数:充当计数器
+# for n in range(1, 6):     # 遵守包前不包后所以是1-5
+#     print(n)
+#
+# # 计算1加到100
+# s = 0
+# for v in range(1, 101):
+#     # print(v)
+#     s = s + v
+#     print(s)
+# print(s)
+#
+# # for比while更简便
+#
+# # break and continue
+# u = 1
+# while u <= 100:
+#     print(u)
+#     u += 1
+#     if u == 3:
+#         print(u)
+#         break
+#
+# d = 0
+# while d <5:
+#     d += 1
+#     if d == 2:
+#         continue
+#     print(d)
+#
+# b= 1
+# for b in range(1, 11):
+#     if b == 5:
+#         break
+#     print(b)
+
+
+# 字符串编码本质上就是二进制数据与语言文字的一一对应关系
